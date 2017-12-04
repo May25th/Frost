3995f2864f4914a71d77f6ecab728bcb0fcfbeba
May25th <214816701@qq.com>
2017-12-04 15:20:48 +0800

    Article

--- blog/models/article.py
+++ blog/models/article.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Article(models.Model):
     author = models.IntegerField(default=0),
     name = models.CharField(max_length=64)
-    text = models.TextField()
+    text = models.TextField(null=True)
     create_at = models.DateTimeField(auto_now_add=True)
     modify_at = models.DateTimeField(auto_now=True)
 
