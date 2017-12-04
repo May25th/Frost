4adb94250f76a0d5e1d9e8a090babbd50807b1b5
May25th <214816701@qq.com>
2017-12-04 15:04:21 +0800

    Article

--- blog/models/book.py
+++ blog/models/book.py
@@ -6,6 +6,6 @@ from django.utils import timezone
 class Book(models.Model):
     t_id = models.CharField(max_length=32, unique=True),
     name = models.CharField(max_length=64)
-    tip = models.CharField(max_length=64)
+    tip = models.CharField(max_length=64, null=True)
     # timestamp = models.DateTimeField(default=timezone.now)
 
